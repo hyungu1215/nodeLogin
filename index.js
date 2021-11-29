29bc888301f545f587329c2a2373a1ef94fd8e95
hyungu1215 <hyungu1215@naver.com>
2021-11-29 19:02:10 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+const config = require("./config/key");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://hyungu1215:phg%4019931215@myfirstnodeproject.sphc6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("MongoDB Connected...");
   })
@@ -20,7 +26,27 @@ mongoose
   });
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! Nice to meet you~");
+});
+
+app.post("/register", (req, res) => {
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그 것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) {
+      return res.json({
+        success: false,
+        err,
+      });
+    } else {
+      return res.status(200).json({
+        success: true,
+      });
+    }
+  });
 });
 
 app.listen(port, () => {
