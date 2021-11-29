bf7860cd94baac3b063aba249451aed22619b121
hyungu1215 <hyungu1215@naver.com>
2021-11-29 16:21:39 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://hyungu1215:phg%4019931215@myfirstnodeproject.sphc6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("MongoDB Connected...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
